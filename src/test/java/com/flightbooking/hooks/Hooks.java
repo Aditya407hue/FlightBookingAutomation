988b9e4fb9af7e8d0bb29164c17de5b46038f743
Aditya407hue <aditya2002sjv@gmail.com>
2025-07-28 17:25:58 +0530

    hooks and runner added

--- src/test/java/com/flightbooking/hooks/Hooks.java
+++ src/test/java/com/flightbooking/hooks/Hooks.java
@@ -1,5 +1,42 @@
 package com.flightbooking.hooks;
 
+import com.flightbooking.utils.ConfigReader;
+import com.flightbooking.utils.LoggerUtil;
+import com.flightbooking.utils.WebDriverFactory;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import java.time.Duration; // Import Duration
+
 public class Hooks {
-    // TODO: Implement Cucumber hooks
+
+    @Before
+    public void setup() {
+        LoggerUtil.info("Setting up WebDriver.");
+        ConfigReader.loadProperties(); // Load properties before tests run
+        WebDriverFactory.initializeDriver();
+        WebDriver driver = WebDriverFactory.getDriver();
+        if (driver != null) {
+            // Implicit wait is already set in WebDriverFactory.initializeDriver()
+            driver.manage().window().maximize();
+        }
+    }
+
+    @After
+    public void teardown(Scenario scenario) {
+        LoggerUtil.info("Tearing down WebDriver.");
+        if (scenario.isFailed()) {
+            try {
+                byte[] screenshot = ((TakesScreenshot) WebDriverFactory.getDriver()).getScreenshotAs(OutputType.BYTES);
+                scenario.attach(screenshot, "image/png", scenario.getName());
+                LoggerUtil.error("Screenshot captured for failed scenario: " + scenario.getName());
+            } catch (Exception e) {
+                LoggerUtil.error("Failed to capture screenshot: " + e.getMessage());
+            }
+        }
+        WebDriverFactory.quitDriver();
+    }
 }
--- src/test/java/com/flightbooking/runners/TestRunner.java
+++ src/test/java/com/flightbooking/runners/TestRunner.java
@@ -1,5 +1,23 @@
 package com.flightbooking.runners;
 
-public class TestRunner {
-    // TODO: Implement TestNG runner for Cucumber
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = "src/test/resources/features", // Path to your feature files
+        glue = {"com.flightbooking.stepDefinitions", "com.flightbooking.hooks"}, // Package where step definitions and hooks are
+        plugin = {"pretty", "html:target/cucumber-reports/cucumber-html-report.html",
+                "json:target/cucumber-reports/cucumber.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, // Reporting plugins
+        monochrome = true, // Readable console output
+        dryRun = false, // True to check if all steps have definitions but don't execute
+        tags = "@Login or @FlightBooking" // Use tags to run specific features/scenarios. Remove for all.
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true) // Enables parallel execution of scenarios
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
